aa9695e6efc87962451bd37d0d2e32e1275aedcc
Yuri Isbener <yurisuhett@gmail.com>
2020-11-13 16:38:23 -0300

    projeto concluido

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+
+            if(idade >= 18){
+                Console.WriteLine("Você gostaria de participar do time de CS GO? - Sim ou Não");
+                string resposta = Console.ReadLine();
+                if (resposta == "Sim"){
+                    Console.WriteLine("Compareça a secretaria - Falar com a Sara");
+                }
+                else{
+                    Console.WriteLine("Passar bem.");
+                }
+            }else{
+              Console.WriteLine("Você gostaria de participar do time de Lol? - Sim ou Não");
+              string resposta = Console.ReadLine();
+              if (resposta == "Sim"){
+                    Console.WriteLine("Compareça a secretaria - Falar com Jucelino");
+                        
+              }else{
+                  Console.WriteLine("Muito Obrigado");
+                        
+              }
+            }
+        }
+    }
+}
